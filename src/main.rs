b203165b27faecda45a79570e01eb97f7569da93
Victor Semenov <suit.uanic@gmail..com>
2018-10-28 05:17:56 +0000

    Relocate setting to ImageProcessorPool

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,6 @@ use router::Router;
 use logger::Logger;
 use params::Params;
 use persistent::{Read, State};
-use iron::typemap::Key;
 use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
@@ -37,11 +36,6 @@ mod crawler;
 mod image_processor;
 
 use image_processor_pool::{ImageProcessorPool, ImageProcessorPoolShared};
-
-//Shared data structures
-#[derive(Copy, Clone)]
-pub struct GallerySettings;
-impl Key for GallerySettings { type Value = HashMap<String, String>; }
 
 fn login_handler(request: &mut Request) -> IronResult<Response> {
 	println!("{:?}", request.get_ref::<Params>());
@@ -72,10 +66,22 @@ fn main() {
 
 	//Create router instance
 	let mut router = Router::new();
-	router.post("/api/add_source_path", crawler::add_source_path, "add_source_path");
-	router.get( "/api/list_source_paths", crawler::list_source_paths, "list_source_paths");
-	router.get( "/api/healthcheck",  healthcheck::get_handler, "healthcheck");
-	router.post("/api/login/:name", login_handler,        "login");
+	router.post("/api/add_source_path",
+		crawler::add_source_path,
+		"add_source_path"
+	);
+	router.get("/api/list_source_paths",
+		crawler::list_source_paths,
+		"list_source_paths"
+	);
+	router.get("/api/healthcheck",
+		healthcheck::get_handler,
+		"healthcheck"
+	);
+	router.post("/api/login/:name",
+		login_handler,
+		"login"
+	);
 	router.post("/api/process_source_path",
 		image_processor::process_source_path,
 		"process_source_path"
@@ -85,9 +91,6 @@ fn main() {
 		"process_status"
 	);
 
-
-
-
 	let mut chain = Chain::new(router);
 	let (logger_before, logger_after) = Logger::new(None);
 	chain.link_before(logger_before);
@@ -95,7 +98,7 @@ fn main() {
 	chain.link_after(logger_after);
 
 	// Shared state init
-	let mut processor_pool = ImageProcessorPool::new();
+	let mut processor_pool = ImageProcessorPool::new(settings);
 
 	//3 is a hypothetical source_id
 	// processor_pool.add_source_to_process(3);
@@ -103,9 +106,11 @@ fn main() {
 	// thread::sleep(Duration::from_secs(1));
 	// println!("Status: {:?}", processor_pool.status_of(3));
 
-	chain.link_before(Read::<GallerySettings>::one(settings));
 	chain.link_before(State::<ImageProcessorPoolShared>::one(processor_pool));
 
-
-	Iron::new(chain).http("0.0.0.0:3000").unwrap();
+	let bind = "0.0.0.0:3000";
+	match Iron::new(chain).http(bind) {
+		Ok(_) => println!("Server binded to {:?}", bind),
+		Err(_) => println!("Server couldn't bind")
+	};
 }
